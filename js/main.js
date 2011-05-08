9718d3055e01d8fc4116e8e52ce71a3d922c1564
Daniel Milde <daniel@milde.cz>
2011-05-08 13:04:28 +0200

    editable ticket detail

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
 		$('#' + id).toggle('slow');
 		return false;
 	});
+
+	$('.toggle-edit').click(function() {
+		var id = $(this).attr('data-target');
+		$('#' + id + '-data').toggle();
+		$('#' + id + '-form').toggle();
+
+		var msg = $(this).html();
+		$(this).html($(this).attr('data-alt'));
+		$(this).attr('data-alt', msg);
+		return false;
+	});
  });
